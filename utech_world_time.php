02746f1478a13c42c7fbe63ed70683cd39ec96be
Meini <Meini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-02 10:13:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/utech-world-time-for-wp/trunk@224310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- utech_world_time.php
+++ utech_world_time.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Utech World Time
 Plugin URI: http://www.utechworld.com/projects/world-time-for-wp/
-Description: Create real time world clocks using simple shortcode. E.g. [worldtime places="Brisbane"]. For more details and to rate this plugin please go to the <a href="http://wordpress.org/extend/plugins/utech-world-time-for-wp/">plugin page</a>.
+Description: Create digital real time world clocks using simple shortcode. E.g. [worldtime places="Brisbane"]. For more details and  <a href="http://wordpress.org/extend/plugins/utech-world-time-for-wp/#compatibility">to rate</a> this plugin please go to the <a href="http://wordpress.org/extend/plugins/utech-world-time-for-wp/">plugin page</a>. Enjoy!
 Version:  0.1.1
 Author: Meini
 Author URI: http://www.utechworld.com
